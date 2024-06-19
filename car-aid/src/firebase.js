13b2a7215be71f31e31bcd025207a8ae6c19a761
Pratham Batra <pratham11.batra@gmail.com>
2024-06-19 16:37:11 +0530

    Initial commit

---
+++ car-aid/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCBug4wQVBgG48frqxio5HebC_ynP_eWPI",
+    authDomain: "caraid-7f0a2.firebaseapp.com",
+    projectId: "caraid-7f0a2",
+    storageBucket: "caraid-7f0a2.appspot.com",
+    messagingSenderId: "846173969900",
+    appId: "1:846173969900:web:1de87ceeaa9326f8bd74c9",
+    measurementId: "G-PMQMVEGGKJ"
+  };
